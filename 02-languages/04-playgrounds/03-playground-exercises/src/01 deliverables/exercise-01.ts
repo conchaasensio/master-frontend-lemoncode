ea5894d5f9436a01e6d7c104b8be1f7e364102fa
Concha Asensio <concha@codium.team>
2022-11-20 18:59:02 +0100

    Add exercise 1

--- 02-languages/04-playgrounds/03-playground-exercises/src/01 deliverables/exercise-01.ts
+++ 02-languages/04-playgrounds/03-playground-exercises/src/01 deliverables/exercise-01.ts
@@ -1,1 +1,34 @@
-console.log("************** DELIVERABLE 01 *********************");
+console.log("************** DELIVERABLE 01 - Array Operations *********************");
+
+//Head
+
+const andalusianCities = ['Almería', 'Granada', 'Córdoba', 'Jaén', 'Málaga', 'Sevilla', 'Cádiz', 'Huelva'];
+
+const head = (list) => {
+  const [first] = list;
+  return first;
+}
+
+console.log(head(andalusianCities));
+
+
+//Tail
+const tail = (list) => {
+  const [, ...rest] = list;
+  return rest;
+}
+
+console.log(tail(andalusianCities));
+
+//Init
+const init = (list) => list.slice(0, -1);
+console.log(init(andalusianCities));
+
+
+//last
+const last = (list) => list.at(-1);
+console.log(last(andalusianCities));
+
+
+
+

2402c9b49d36a37cf92ca5d574a132caadfa7d66
Concha Asensio Martínez-Rives <conchaasensio@MacBook-Pro-de-Concha.local>
2022-12-04 18:00:03 +0100

    Add exercise 2

--- 02-languages/04-playgrounds/03-playground-ejercicios/src/02-entregables/ejercicio-02.ts
+++ 02-languages/04-playgrounds/03-playground-ejercicios/src/02-entregables/ejercicio-02.ts
@@ -1,1 +1,18 @@
 console.log("************** DELIVERABLE 02 *********************");
+
+//Concat
+
+const boyNames = ['Pablo', 'Martín', 'Miguel'];
+const girlNames = ['Sofía', 'Irene', 'Laura'];
+
+const concat = (a, b) => [...a, ...b];
+
+console.log(concat(boyNames, girlNames));
+
+//Opcional
+
+const neutralNames = ['Mati', 'Nico', 'Noah'];
+
+const concatMultiple = (...args) => args.reduce((acc, element) => concat(acc, element), []);
+
+console.log(concatMultiple(boyNames, girlNames, neutralNames));

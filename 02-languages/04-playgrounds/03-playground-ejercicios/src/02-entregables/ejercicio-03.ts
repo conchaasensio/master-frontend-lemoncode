9345360e58857096ac40384d9aa7698db08399b5
Concha Asensio Martínez-Rives <conchaasensio@MacBook-Pro-de-Concha.local>
2022-12-04 18:29:13 +0100

    Add exercise 3

--- 02-languages/04-playgrounds/03-playground-ejercicios/src/02-entregables/ejercicio-03.ts
+++ 02-languages/04-playgrounds/03-playground-ejercicios/src/02-entregables/ejercicio-03.ts
@@ -1,1 +1,23 @@
 console.log("************** DELIVERABLE 03 *********************");
+
+//Clone
+const source = {
+  firstName: 'Concha',
+  lastName: 'Asensio',
+  country: 'Spain'
+}
+
+const clone = (source) => ({...source});
+
+console.log(clone(source));
+
+//Merge
+const target = {
+  firstName: 'Luis',
+  age: 40,
+  married: true
+}
+
+const merge = (source, target) => ({...target, ...source});
+
+console.log(merge(source, target));

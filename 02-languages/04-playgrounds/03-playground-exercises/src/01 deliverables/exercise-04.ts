56a5926ee683253785d73ac49c98978c3d9b5e8f
Concha Asensio Martínez-Rives <conchaasensio@MacBook-Pro-de-Concha.local>
2022-12-04 19:03:03 +0100

    Add exercise 4

--- 02-languages/04-playgrounds/03-playground-exercises/src/01 deliverables/exercise-04.ts
+++ 02-languages/04-playgrounds/03-playground-exercises/src/01 deliverables/exercise-04.ts
@@ -1,1 +1,14 @@
 console.log("************** DELIVERABLE 04 *********************");
+
+const books = [
+  {title: 'Los renglones torcidos de Dios', isRead: true},
+  {title: 'El nombre de la rosa', isRead: true},
+  {title: 'Juego de tronos', isRead: false},
+  {title: 'Crepúsculo', isRead: false}
+]
+
+const isBookRead = (books, title) => books.find(book => book.title === title)?.isRead || false;
+
+console.log(isBookRead(books, 'Los renglones torcidos de Dios'));
+console.log(isBookRead(books, 'Juego de tronos'));
+console.log(isBookRead(books, 'Conan'));

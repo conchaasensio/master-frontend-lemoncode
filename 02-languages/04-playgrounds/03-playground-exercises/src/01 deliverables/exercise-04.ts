f0799d2c621ff33c78ff0547a71c938800760fcc
Concha Asensio Martínez-Rives <conchaasensio@MacBook-Pro-de-Concha.local>
2022-12-05 19:18:06 +0100

    Add exercise 4 resolution using TS

--- 02-languages/04-playgrounds/03-playground-exercises/src/01 deliverables/exercise-04.ts
+++ 02-languages/04-playgrounds/03-playground-exercises/src/01 deliverables/exercise-04.ts
@@ -1,14 +1,35 @@
 console.log("************** DELIVERABLE 04 *********************");
 
-const books = [
+// const books = [
+//   {title: 'Los renglones torcidos de Dios', isRead: true},
+//   {title: 'El nombre de la rosa', isRead: true},
+//   {title: 'Juego de tronos', isRead: false},
+//   {title: 'Crepúsculo', isRead: false}
+// ]
+
+// const isBookRead = (books, title) => books.find(book => book.title === title)?.isRead || false;
+
+// console.log(isBookRead(books, 'Los renglones torcidos de Dios'));
+// console.log(isBookRead(books, 'Juego de tronos'));
+// console.log(isBookRead(books, 'Conan'));
+
+//Optional with TS
+
+type BookTitle = string;
+
+interface Book {
+  title: BookTitle,
+  isRead: boolean
+}
+
+const books: Book [] = [
   {title: 'Los renglones torcidos de Dios', isRead: true},
   {title: 'El nombre de la rosa', isRead: true},
   {title: 'Juego de tronos', isRead: false},
   {title: 'Crepúsculo', isRead: false}
 ]
 
-const isBookRead = (books, title) => books.find(book => book.title === title)?.isRead || false;
+const isBookRead = (books: Book[], title: BookTitle): boolean => books.find((book: Book) => book.title === title)?.isRead || false;
 
-console.log(isBookRead(books, 'Los renglones torcidos de Dios'));
-console.log(isBookRead(books, 'Juego de tronos'));
-console.log(isBookRead(books, 'Conan'));
+
+console.log(isBookRead(books, 'El nombre de la rosa'));

118d60eff690830dd50721c7b3a13f5773fb7202
Concha Asensio <concha@codium.team>
2023-07-20 09:36:11 +0200

    Import styles

--- 03-bundling/08-deliverable/src/index.tsx
+++ 03-bundling/08-deliverable/src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import logo from './content/logo_1.png';
+import './styles.scss';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <h1>Hello World!</h1>
+    <h1>¡Hola, mundo!</h1>
     <img src={logo} />
   </div>
 );
